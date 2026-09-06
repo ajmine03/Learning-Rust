7a1b4de776649819e3426a44a5f82bc46517bd20
Ajmine Adil <ajmineadil24@gmail.com>
2026-09-07 00:50:08 +0600

    nothing cngs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+// fn main(){
+//     println!("hey ajmine!\nit's 5/9/26 !");
+//     println!("Best of Luck\n");
+
+
+
+// }
+
 fn main(){
-    println!("hey ajmine!\nit's 5/9/26 !");
-    println!("Best of Luck\n");
-
-
+    let s1 = String::from("Hello");
     
 }
 
